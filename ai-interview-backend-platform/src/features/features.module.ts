223ce79d7cd3ec1826a6fce7e30697fa3175607a
Zeenat Bano <zeenatjjn2003@gmail.com>
2025-05-05 00:01:33 +0530

    basic submission feature

--- ai-interview-backend-platform/src/features/features.module.ts
+++ ai-interview-backend-platform/src/features/features.module.ts
@@ -2,15 +2,18 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { QuestionModule } from './question/question.module';
+import { SubmissionModule } from './submission/submission.module';
 
 @Module({
   imports: [
     UserModule,
     QuestionModule,
+    SubmissionModule
   ],
   exports: [
     UserModule,
     QuestionModule,
+    SubmissionModule
   ],
 })
 export class FeaturesModule {}
